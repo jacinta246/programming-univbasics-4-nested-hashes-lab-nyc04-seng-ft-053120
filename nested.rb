8db617969aa69c4dfe7683df8454def3d0082872
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-15 01:13:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -53,7 +53,7 @@ def dennis_ritchies_language
         :known_for => "Unix",
         :languages => ["C"]
       },
-
+[:dennis_ritchie][:languages]
     }
 
 end
@@ -81,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-[:yukihiro_matsumoto][:languages]
+[:yukihiro_matsumoto]
 end
 
 def changing_alan
