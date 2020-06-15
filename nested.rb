2e156f606e5bc37505e8a2459e313a2a1637b54a
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-15 01:06:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -53,7 +53,10 @@ def dennis_ritchies_language
         :known_for => "Unix",
         :languages => ["C"]
       },
-      :yukihiro_matsumoto => {}
+      :yukihiro_matsumoto => {
+				:known_for=>
+				:languages =>
+			}
     }
 
 end
